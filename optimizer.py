1e1137b2cdba4ce50e5b532ddd42532235edeff9
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-06-16 12:37:01 +0530

    feat: initial commit

---
+++ optimizer.py
@@ -0,0 +1,20 @@
+"""
+We use an optimizer to adjust parameters of our network
+based on the gradients computed during backpropagation
+"""
+
+from nn import NeuralNet
+
+
+class Optimizer:
+    def step(self, ned: NeuralNet) -> None:
+        raise NotImplementedError
+
+
+class SGD(Optimizer):
+    def __init__(self, lr: float = 0.01):
+        self.lr = lr
+
+    def step(self, net: NeuralNet) -> None:
+        for param, grad in net.params_and_grads():
+            param -= self.lr * param
